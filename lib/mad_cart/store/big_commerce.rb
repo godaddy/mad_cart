d17f16f0d8d70e72f4f71f5a1b26e32bb4b86a4c
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2015-06-01 11:23:57 +0200

    Specify connection open and read timeouts

--- lib/mad_cart/store/big_commerce.rb
+++ lib/mad_cart/store/big_commerce.rb
@@ -109,7 +109,7 @@ module MadCart
       end
 
       def create_connection(args={})
-        Faraday.new(:url => api_url_for(args[:store_url])) do |connection|
+        Faraday.new(DEFAULT_CONNECTION_OPTIONS.merge(:url => api_url_for(args[:store_url]))) do |connection|
           connection.basic_auth(args[:username], args[:api_key])
           connection.response :json
           connection.adapter Faraday.default_adapter
